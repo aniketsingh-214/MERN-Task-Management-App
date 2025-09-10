0063153624ead27c7b98c2f6cede390ceffcf006
Aniket Singh <aniketsingh7141340@gmail.com>
2025-09-10 15:33:58 +0530

    project upload

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  const isProduction = process.env.NODE_ENV === "production";
+
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: isProduction, 
+    sameSite: isProduction ? "None" : "Lax", 
+    domain: isProduction ? "onrender.com" : undefined, 
+    path: "/",
+    maxAge: 24 * 60 * 60 * 1000, 
+  });
+
+  return token;
+};
+
+export default generateToken;
---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+
+function Button({
+  children,
+  bgColor = "bg-blue-600",
+  textColor,
+  className = "",
+  ...props
+}) {
+  return (
+    <button
+      className={`px-4 py-2 ${bgColor} ${textColor} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
