432f2f9462da55d1b4bcbde53050e5fa5179de54
ANIKET SINGH <125822570+aniketsingh-214@users.noreply.github.com>
2025-09-13 15:17:40 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['axios', 'react-router-dom'] 
+        }
+      }
+    }
+  }
 })
